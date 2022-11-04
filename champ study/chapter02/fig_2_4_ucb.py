7c4d87e7fafd230eab9655ed4af90564382a76d1
Mohamad Nawfal <aiexpert2025@gmail.com>
2022-11-04 11:51:47 -0700

    ...

--- champ study/chapter02/fig_2_4_ucb.py
+++ champ study/chapter02/fig_2_4_ucb.py
@@ -15,43 +15,49 @@ class Bandit:
     def __init__(self, n_timesteps, n_arms):
         self.arms = list(range(n_arms))
         self.q = np.random.normal(loc=0, scale=1, size=10)
-        self.rewards = [np.random.normal(q, 1, n_timesteps) for q in self.q]
 
     def reward(self, arm, timestep):
-        return self.rewards[arm][timestep]
+        return np.random.normal(self.q[arm], 1)
 
 
-def simulate(n_bandits=2000, n_timesteps=1000, n_arms=10, e=0.0, alpha=0.1, init_type="realistic"):
+def simulate(n_bandits=2000, n_timesteps=1000, n_arms=10, e=0.0, alpha=0.1, act_type="e-greedy", c=2.0):
     reward_grid = np.zeros((n_bandits, n_timesteps))
     optimality_grid = np.zeros((n_bandits, n_timesteps))
     for b in tqdm(range(n_bandits)):
         bandit = Bandit(n_timesteps, n_arms)
-        Q = np.ones(n_arms) * 5 if init_type == 'optimistic' else np.zeros(n_arms)
-        for t in range(n_timesteps):
+        Q = np.zeros(n_arms)
+        N = np.ones(n_arms)
+        for t in range(1, n_timesteps + 1):
             optimal_action = np.argmax(bandit.q)
-            if np.random.rand() < e:
-                action = np.random.choice(bandit.arms)
+            if act_type == "e-greedy":
+                if np.random.rand() < e:
+                    action = np.random.choice(bandit.arms)
+                else:
+                    max_val = np.max(Q)
+                    action = np.random.choice(np.where(Q == max_val)[0])
             else:
-                max_val = np.max(Q)
-                action = np.random.choice(np.where(Q == max_val)[0])
+                uncertainty = np.sqrt(np.log(t) / N)
+                ucb = Q + c * uncertainty
+                action = np.argmax(ucb)
             reward = bandit.reward(action, t)
+            N[action] += 1
             Q[action] = Q[action] + alpha * (reward - Q[action])
-            reward_grid[b][t] = reward
+            reward_grid[b][t-1] = reward
             if action == optimal_action:
-                optimality_grid[b][t] = 1
+                optimality_grid[b][t-1] = 1
     return reward_grid, optimality_grid
 
 
 def fig_2_3():
     fig, ax = plt.subplots(2, 1, figsize=(10, 10))
     epsilons = [0, 0.1]
-    init_types = ["optimistic", "realistic"]
+    init_types = ["ucb", "e-greedy"]
     for e, i in zip(epsilons, init_types):
-        rewards, optimality = simulate(e=e, init_type=i)
+        rewards, optimality = simulate(e=e, act_type=i)
         rewards_mean = np.mean(rewards, axis=0)
         optimal_selection = np.mean(optimality, axis=0)
-        ax[0].plot(rewards_mean, label=f'{i}, epsilon = {e}')
-        ax[1].plot(optimal_selection, label=f'{i}, epsilon = {e}')
+        ax[0].plot(rewards_mean, label=f'{i}')
+        ax[1].plot(optimal_selection, label=f'{i}')
     ax[0].legend()
     ax[1].legend()
     # plt.show()
